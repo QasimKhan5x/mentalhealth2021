454a4445769143e124d280a75286176fc62f2761
FahadZaheerfzr <fahadzaheer19@gmail.com>
2021-05-30 04:52:42 +0500

    About us, Settings, Calendar, Profile Added

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
 import AddEntryComponent from './addEntry/add-entry/add-entry.component';
 import { AllEntriesComponent } from './allEntries/all-entries/all-entries.component';
+import { CalendarComponent } from './calendar/calendar.component';
 import { HomeComponent } from './home/home/home.component';
 import { LoginComponent } from './login/login/login.component';
 import { RegisterComponent } from './register/register/register.component';
+import { SettingsComponent } from './settings/settings.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 
 const routes: Routes = [
@@ -13,7 +17,10 @@ const routes: Routes = [
   { path: "register", component: RegisterComponent },
   { path: 'allentries', component: AllEntriesComponent },
   { path: "addentry", component: AddEntryComponent },
-  
+  { path: "about-us", component: AboutUsComponent },
+  { path: "calendar", component: CalendarComponent },
+  {path: "my_profile", component: UserProfileComponent  },
+  {path: "settings", component: SettingsComponent  },
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -11,25 +11,45 @@ import { RegisterComponent } from './register/register/register.component'
 ;
 import AddEntryComponent from './addEntry/add-entry/add-entry.component'
 ;
-import { HomeComponent } from './home/home/home.component'
-;
-import {  AllEntriesComponent} from './allEntries/all-entries/all-entries.component'
+import { HomeComponent } from './home/home/home.component';
+
+import { MatFormFieldControl, MatFormFieldModule } from '@angular/material/form-field';
+import {  AllEntriesComponent} from './allEntries/all-entries/all-entries.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { CalendarComponent } from './calendar/calendar.component'
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';;
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { SettingsComponent } from './settings/settings.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SidebarComponent
-,
-    LoginComponent
-,
+    SidebarComponent,
+    LoginComponent,
     RegisterComponent ,
     AddEntryComponent,
     AllEntriesComponent,
+    AboutUsComponent,
+    CalendarComponent,
+    UserProfileComponent,
+    SettingsComponent,
     HomeComponent],
+
+ 
+  
+
 
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatFormFieldModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
