78aec8c36004de461671a6c0bf4adf0a82b72c09
zShan Ahmad <mahmad.zeeshan72@gmail.com>
2021-05-29 09:58:41 +0500

    add user

--- frontend/src/app/services/login.service.ts
+++ frontend/src/app/services/login.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { baseUrl } from '../_shared/baseUrl';
+import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  authenticate(id: string, key: string): boolean {
+    let u = this.http.get<User>(baseUrl + '/users/' + id);
+    console.log(u);
+    return true;
+  }
 }
