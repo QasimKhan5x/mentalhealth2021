20eb3573cd9454bb3765dac01e4fd20ccfe3451f
QasimKhan5x <straightouttadetroit.qk@gmail.com>
2021-06-13 21:30:44 +0500

    upload pp to fb

--- frontend/src/app/user-profile/user-profile.component.ts
+++ frontend/src/app/user-profile/user-profile.component.ts
@@ -1,18 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireStorage } from '@angular/fire/storage';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.css']
+  styleUrls: ['./user-profile.component.css'],
 })
 export class UserProfileComponent implements OnInit {
+  path?: File;
 
-  constructor() {
-   
+  constructor(private fireStorage: AngularFireStorage) {}
+
+  ngOnInit() {}
+
+  upload($event: any): void {
+    this.path = $event.target.files[0];
   }
 
-  ngOnInit() {
-    
+  uploadImage(): void {
+    console.log(this.path?.name);
+    this.fireStorage.upload(
+      '/profilepics/' + Math.random() + this.path?.name,
+      this.path
+    );
   }
-
 }
