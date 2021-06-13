8329a7b4cdd6474d52b56b429ebafd01b692d619
QasimKhan5x <straightouttadetroit.qk@gmail.com>
2021-06-13 20:52:27 +0500

    remove useless files

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,39 +1,38 @@
 ﻿import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {
-  MatFormFieldControl,
-  MatFormFieldModule,
-} from '@angular/material/form-field';
-
-// Firebase modules
-import { AngularFireStorageModule } from '@angular/fire/storage';
-import { AngularFireModule } from '@angular/fire';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header/header.component';
 import { SidebarComponent } from './sidebar/sidebar/sidebar.component';
+import {
+  MatFormFieldControl,
+  MatFormFieldModule,
+} from '@angular/material/form-field';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { CalendarComponent } from './calendar/calendar.component';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { SettingsComponent } from './settings/settings.component';
+
 import { LoginComponent } from './login/login/login.component';
 import { RegisterComponent } from './register/register/register.component';
 import AddEntryComponent from './addEntry/add-entry/add-entry.component';
 import { HomeComponent } from './home/home/home.component';
 import { AllEntriesComponent } from './allEntries/all-entries/all-entries.component';
-
+import { HttpClientModule } from '@angular/common/http';
 import { LoginService } from './services/login.service';
 import { SignupService } from './services/signup.service';
 import { ErrorService } from './services/error.service';
 import { EntryService } from './services/entry.service';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { RefreshComponent } from './refresh/refresh.component';
+
+// Firebase modules
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireModule } from '@angular/fire';
 
 @NgModule({
   declarations: [
@@ -52,6 +51,7 @@ import { RefreshComponent } from './refresh/refresh.component';
     RefreshComponent,
     HomeComponent,
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule,
