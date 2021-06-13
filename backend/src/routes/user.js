fe03de8507c79f034f0d2ca3ca6462ff6312d9e1
QasimKhan5x <straightouttadetroit.qk@gmail.com>
2021-06-13 15:21:07 +0500

    email routes

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -8,11 +8,17 @@ const user = require('@controllers/user.js');
 
 router.get('/api/users/:id', user.getUserById);
 
+router.get('/api/users/:email', user.getUserByEmail);
+
 router.get('/api/users', user.getUsers);
 
 router.post('/api/users', user.newUser);
 
 router.delete('/api/users/:id', user.deleteUser);
+
+router.delete('/api/users/:email', user.deleteUserByEmail);
+
+router.patch('/api/users/:email', user.updateUserByEmail);
 
 router.patch('/api/users/:id', user.updateUser);
 
